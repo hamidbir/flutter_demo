f7f0b694398ae8dedb027013d12b5a8188c3f46c
undefined <oncwnuFWiuizeNvV3LymoBjk_Hy8@git.weixin.qq.com>
2019-10-22 15:14:04 +0800

    base_demo

--- lib/basic/basic_demo.dart
+++ lib/basic/basic_demo.dart
@@ -4,6 +4,90 @@ class BasicDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text('hello');
+    return Container(
+      // color: Colors.grey[100],
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage('https://resources.ninghao.org/images/say-hello-to-barry.jpg'),
+          alignment: Alignment.topCenter,
+          // repeat: ImageRepeat.repeatY,
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.indigoAccent[400].withOpacity(0.5),
+            BlendMode.hardLight,
+          ),
+        ),
+      ),
+      child: Row(
+        children: <Widget>[
+          Container(
+            child: Icon(Icons.pool, size: 32.0, color: Colors.white),
+            padding: EdgeInsets.fromLTRB(10,10,10,10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.red[200],
+              border: Border.all(
+                color: Colors.green[100],
+                width: 3,
+              ),
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                colors: [
+                  Colors.red,
+                  Colors.green
+                ]
+              )
+              // borderRadius: BorderRadius.circular(10)
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RichTextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return RichText(
+      text: TextSpan(
+        text: 'liangrr',
+        style: TextStyle(
+          color: Colors.blue[300],
+          fontSize: 20.0
+        ),
+        children: [
+          TextSpan(
+            text: '.xyz',
+            style: TextStyle(
+              color: Colors.red
+            )
+          )
+        ]
+      ),
+    );
+  }
+}
+
+class TextDemo extends StatelessWidget {
+
+  final TextStyle _textStyle = TextStyle(
+    fontSize: 16.0
+  );
+
+  final String _author = '杜埔';
+  final String _title = '黄鹤楼';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text(
+      'hello $_title 这是啥子哦这是啥子哦这是啥子哦这是',
+      textAlign: TextAlign.center,
+      style: _textStyle,
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+    );
   }
 }
--- lib/components/bottom_navigation_demo.dart
+++ lib/components/bottom_navigation_demo.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_tab_bar/bottom_tab_bar.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavigationBarDemo extends StatefulWidget {
@@ -10,22 +11,50 @@ class BottomNavigationBarDemo extends StatefulWidget {
 
 class _BottomNavigationBarDemoState extends State<BottomNavigationBarDemo> {
   int _currentIndex = 0;
+  String badgeNo1;
+  var titles = [ '首页', '附近机构', '应用', '我的' ];
+  var icons = [ Icons.home, Icons.details, Icons.apps, Icons.person ];
+
   void _onTapHandler (int index) {
     setState(() {
       _currentIndex = index;
     });
   }
+
+  void initState() {
+    super.initState();
+    badgeNo1 = '12';
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return BottomNavigationBar(
+    return BottomTabBar(
       currentIndex: _currentIndex,
+      type: BottomTabBarType.fixed,
+      fixedColor : Colors.yellow[300],
       onTap: _onTapHandler,
+      isAnimation: false,
+      badgeColor: Colors.red,
       items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-        BottomNavigationBarItem(icon: Icon(Icons.details), title: Text('Details')),
-        BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('Person'))
+        BottomTabBarItem(icon: Icon(icons[0]), title: Text(titles[0]), badgeNo: badgeNo1),
+        BottomTabBarItem(icon: Icon(icons[1]), title: Text(titles[1]), badgeNo: '1'),
+        BottomTabBarItem(icon: Icon(icons[2]), title: Text(titles[2]), badgeNo: '14'),
+        BottomTabBarItem(icon: Icon(icons[3]), title: Text(titles[3]), badgeNo: '33'),
       ],
     );
+    // return BottomNavigationBar(
+    //   currentIndex: _currentIndex,
+    //   backgroundColor: Colors.green,
+    //   onTap: _onTapHandler,
+    //   selectedItemColor: Colors.yellow[300],
+    //   unselectedItemColor: Colors.black26,
+    //   items: [
+        // BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('首页'), ),
+        // BottomNavigationBarItem(icon: Icon(Icons.details), title: Text('附近机构')),
+        // BottomNavigationBarItem(icon: Icon(Icons.apps), title: Text('应用')),
+        // BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('我的'))
+    //   ],
+    // );
   }
 }
+
